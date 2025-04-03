fbd777a093441d4de8ea10eabc2e1dc4297bd231
JoeMinKyung <mk981015@naver.com>
2025-04-03 16:10:29 +0900

    feat(#3, #6, #20, #21): ShowResponse 생성

---
+++ src/main/java/com/example/picket/domain/show/dto/ShowResponse.java
@@ -0,0 +1,59 @@
+package com.example.picket.domain.show.dto;
+
+import com.example.picket.domain.show.entity.Show;
+import com.example.picket.domain.show.entity.ShowDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+public class ShowResponse {
+
+    private Long id;
+    private String title;
+    private String posterUrl;
+    private String category;
+    private String description;
+    private String location;
+    private String reservationStart;
+    private String reservationEnd;
+    private Integer ticketsLimitPerUser;
+    private List<ShowDateResponse> showDates;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime deletedAt;
+
+    public ShowResponse(Show show, List<ShowDate> showDates) {
+        this.id = show.getId();
+        this.title = show.getTitle();
+        this.posterUrl = show.getPosterUrl();
+        this.category = show.getCategory().name();
+        this.description = show.getDescription();
+        this.location = show.getLocation();
+        this.reservationStart = show.getReservationStart().toString();
+        this.reservationEnd = show.getReservationEnd().toString();
+        this.ticketsLimitPerUser = show.getTicketsLimitPerUser();
+        this.showDates = showDates.stream()
+                .map(ShowDateResponse::from)
+                .collect(Collectors.toList());
+
+        this.createdAt = show.getCreatedAt();
+        this.modifiedAt = show.getModifiedAt();
+        this.deletedAt = show.getDeletedAt();
+    }
+
+    public static ShowResponse from(Show show, List<ShowDate> showDates) {
+        return new ShowResponse(show, showDates);
+    }
+}
