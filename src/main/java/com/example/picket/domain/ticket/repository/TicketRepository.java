cf7875c288217365270814720e1fe7dbcc61a3b8
Moon YuBin <yubin5744@gmail.com>
2025-04-03 22:33:13 +0900

    chore(#2) : Errorcode 세미콜론 수정, SeatRepository 함수 추가

--- src/main/java/com/example/picket/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/example/picket/domain/ticket/repository/TicketRepository.java
@@ -1,8 +1,10 @@
 package com.example.picket.domain.ticket.repository;
 
+import com.example.picket.domain.seat.entity.Seat;
 import com.example.picket.domain.ticket.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
+    boolean existsBySeat(Seat seat);
 }
