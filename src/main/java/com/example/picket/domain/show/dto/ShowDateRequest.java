1a34b7446ef2c82c2b39e41d836eecb8a272a97e
JoeMinKyung <mk981015@naver.com>
2025-04-03 17:30:50 +0900

    feat(#3, #6, #20, #21): 유효성 검사 추가

--- src/main/java/com/example/picket/domain/show/dto/ShowDateRequest.java
+++ src/main/java/com/example/picket/domain/show/dto/ShowDateRequest.java
@@ -1,6 +1,8 @@
 package com.example.picket.domain.show.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import java.time.LocalDate;
@@ -9,18 +11,20 @@ import java.time.LocalTime;
 @Getter
 public class ShowDateRequest {
 
-    @NotNull
+    @NotNull(message = "공연 날짜는 필수 입력값입니다.")
+    @FutureOrPresent(message = "공연 날짜는 오늘 이후여야 합니다.")
     private LocalDate date;
 
-    @NotNull
+    @NotNull(message = "공연 시작 시간은 필수 입력값입니다.")
     @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime startTime;
 
-    @NotNull
+    @NotNull(message = "공연 종료 시간은 필수 입력값입니다.")
     @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime endTime;
 
-    @NotNull
+    @NotNull(message = "좌석 수는 필수 입력값입니다.")
+    @Min(value = 1, message = "좌석 수는 1 이상이어야 합니다.")
     private Integer totalSeatCount;
 }
 
