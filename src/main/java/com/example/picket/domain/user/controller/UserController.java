0f4cad940bf07b0139ae748b18ff0b427f764323
dawon <aodlstory321@gmail.com>
2025-04-04 13:20:12 +0900

    chore: Auth 추가

--- src/main/java/com/example/picket/domain/user/controller/UserController.java
+++ src/main/java/com/example/picket/domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.picket.domain.user.controller;
 
+import com.example.picket.common.annotation.Auth;
+import com.example.picket.common.dto.AuthUser;
 import com.example.picket.domain.user.dto.request.UpdatePasswordRequest;
 import com.example.picket.domain.user.dto.request.UpdateUserRequest;
 import com.example.picket.domain.user.dto.request.WithdrawUserRequest;
@@ -16,22 +18,25 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/v1/users")
-    public ResponseEntity<UserResponse> getUser() {
-        return ResponseEntity.ok(userService.getUser());
+    public ResponseEntity<UserResponse> getUser(@Auth AuthUser authUser) {
+        return ResponseEntity.ok(userService.getUser(authUser));
     }
 
     @PatchMapping("/api/v1/users")
-    public ResponseEntity<UserResponse> updateUser(@RequestBody UpdateUserRequest request) {
-        return ResponseEntity.ok(userService.updateUser(request));
+    public ResponseEntity<UserResponse> updateUser(@Auth AuthUser authUser,
+                                                   @RequestBody UpdateUserRequest request) {
+        return ResponseEntity.ok(userService.updateUser(authUser, request));
     }
 
     @PatchMapping("/api/v1/users/password")
-    public void updatePassword(@RequestBody UpdatePasswordRequest request) {
-        userService.updatePassword(request);
+    public void updatePassword(@Auth AuthUser authUser,
+                               @RequestBody UpdatePasswordRequest request) {
+        userService.updatePassword(authUser, request);
     }
 
     @DeleteMapping("/api/v1/users/withdraw")
-    public void withdrawUser(@RequestBody WithdrawUserRequest request) {
-        userService.withdrawUserRequest(request);
+    public void withdrawUser(@Auth AuthUser authUser,
+                             @RequestBody WithdrawUserRequest request) {
+        userService.withdrawUserRequest(authUser, request);
     }
 }
