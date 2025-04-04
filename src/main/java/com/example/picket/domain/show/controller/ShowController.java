8c25103ab6d15637f015cc5c010759bf6f2d0cfe
JoeMinKyung <mk981015@naver.com>
2025-04-04 13:29:54 +0900

    chore(#3, #6, #20, #21): AuthUser 도입 및 RequestMapping 추가 및 분리된 Service명으로 수정

--- src/main/java/com/example/picket/domain/show/controller/ShowController.java
+++ src/main/java/com/example/picket/domain/show/controller/ShowController.java
@@ -1,23 +1,34 @@
 package com.example.picket.domain.show.controller;
 
+import com.example.picket.common.annotation.Auth;
+import com.example.picket.common.annotation.AuthPermission;
+import com.example.picket.common.dto.AuthUser;
+import com.example.picket.common.enums.UserRole;
 import com.example.picket.domain.show.dto.ShowCreateRequest;
 import com.example.picket.domain.show.dto.ShowResponse;
-import com.example.picket.domain.show.service.ShowService;
+import com.example.picket.domain.show.service.ShowCommandService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ShowController {
-    private final ShowService showService;
 
-    @PostMapping("/api/v1/admin/shows")
-    public ResponseEntity<ShowResponse> createShow(@Valid @RequestBody ShowCreateRequest request) {
-        ShowResponse response = showService.createShow(request);
+    private final ShowCommandService showCommandService;
+
+    @PostMapping("/admin/shows")
+    @AuthPermission(role = UserRole.ADMIN)
+    public ResponseEntity<ShowResponse> createShow(
+            @Auth AuthUser authUser,
+            @RequestBody @Valid ShowCreateRequest request
+    ) {
+        ShowResponse response = showCommandService.createShow(authUser, request);
         return ResponseEntity.ok(response);
     }
 }
