b6c18223b37532a2a6a2ef082398c9038da1cc4e
cjh0412 <jyedev04@gmail.com>
2025-04-04 13:17:43 +0900

    feat(#5): 댓글 수정 기능 구현

--- src/main/java/com/example/picket/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/picket/domain/comment/controller/CommentController.java
@@ -4,7 +4,7 @@ import com.example.picket.common.annotation.Auth;
 import com.example.picket.common.dto.AuthUser;
 import com.example.picket.domain.comment.dto.request.CommentRequest;
 import com.example.picket.domain.comment.dto.response.CommentResponse;
-import com.example.picket.domain.comment.service.CommentService;
+import com.example.picket.domain.comment.service.CommentCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/")
 public class CommentController {
 
-    private final CommentService commentService;
+    private final CommentCommandService commentCommandService;
 
     @PostMapping("/shows/{showId}/comments")
     public ResponseEntity<CommentResponse> createComment(@Auth AuthUser auth, @PathVariable Long showId, @RequestBody CommentRequest commentRequest) {
-        return ResponseEntity.ok(commentService.createComment(auth.getId(), showId, commentRequest));
+        return ResponseEntity.ok(commentCommandService.createComment(auth.getId(), showId, commentRequest));
     }
+
+    @PatchMapping("/shows/{showId}/comments/{commentId}")
+    public ResponseEntity<CommentResponse> updateComment(@Auth AuthUser auth, @PathVariable Long showId, @PathVariable Long commentId, @RequestBody CommentRequest commentRequest) {
+        return ResponseEntity.ok(commentCommandService.updateComment(auth.getId(), showId, commentId, commentRequest));
+    }
+
+
 }
