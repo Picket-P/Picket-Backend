20dd19361f33d54bd38248032c79ea90c0d4bef7
JoeMinKyung <mk981015@naver.com>
2025-04-04 14:51:22 +0900

    feat(#3): 공연 목록 조회 추가용 메서드 추가

--- src/main/java/com/example/picket/domain/show/repository/ShowRepository.java
+++ src/main/java/com/example/picket/domain/show/repository/ShowRepository.java
@@ -1,7 +1,12 @@
 package com.example.picket.domain.show.repository;
 
+import com.example.picket.common.enums.Category;
 import com.example.picket.domain.show.entity.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShowRepository extends JpaRepository<Show, Long> {
+
+    List<Show> findAllByCategory(Category category);
 }
