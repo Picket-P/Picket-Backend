1bfcdbd215899e79de610ebd23ae519c85cfb2ad
JoeMinKyung <mk981015@naver.com>
2025-04-07 12:04:06 +0900

    chore(#21): import 제거

--- src/main/java/com/example/picket/domain/show/repository/ShowRepository.java
+++ src/main/java/com/example/picket/domain/show/repository/ShowRepository.java
@@ -3,11 +3,8 @@ package com.example.picket.domain.show.repository;
 import com.example.picket.common.enums.Category;
 import com.example.picket.domain.show.entity.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ShowRepository extends JpaRepository<Show, Long> {
 
