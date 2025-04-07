48a45ff06f495884a583046d0227f40926560f56
JEONTAK <tak980418@naver.com>
2025-04-07 12:24:35 +0900

    refactor(#27, #28, #29) : WHITE_LIST 및 인터셉터 수정

--- src/main/java/com/example/picket/common/consts/Const.java
+++ src/main/java/com/example/picket/common/consts/Const.java
@@ -1,12 +1,28 @@
 package com.example.picket.common.consts;
 
-import java.util.List;
+import java.util.Map;
 
 public interface Const {
 
     String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$";
-    
-    //TODO : 추후 로그인 없이도 방문가능한 페이지의 경우 해당 화이트 리스트에 URL 추가
-    List<String> WHITE_LIST = List.of("/api/v1/auth/signup/*", "/api/v1/auth/signin");
 
+    //TODO : 추후 로그인 없이도 방문가능한 페이지의 경우 해당 화이트 리스트에 URL 추가
+    Map<String, String[]> WHITE_LIST = Map.of(
+            "GET", new String[]{
+
+            },
+            "POST", new String[]{
+                    "/api/v1/auth/signup/*",
+                    "/api/v1/auth/signin",
+            },
+            "PUT", new String[]{
+
+            },
+            "PATCH", new String[]{
+
+            },
+            "DELETE", new String[]{
+
+            }
+    );
 }
--- src/main/java/com/example/picket/common/interceptor/AuthInterceptor.java
+++ src/main/java/com/example/picket/common/interceptor/AuthInterceptor.java
@@ -1,11 +1,13 @@
 package com.example.picket.common.interceptor;
 
+import com.example.picket.common.consts.Const;
 import com.example.picket.common.exception.CustomException;
 import com.example.picket.common.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
+import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -15,10 +17,26 @@ public class AuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         HttpSession session = request.getSession(false);
 
+        String requestMethod = request.getMethod();
+        String requestUri = request.getRequestURI();
+
+        if (isWhiteList(requestMethod, requestUri)) {
+            return true;
+        }
+
         if (session == null) {
             throw new CustomException(ErrorCode.AUTH_UNAUTHORIZED_LOGIN);
         }
 
         return true;
+    }
+
+    private boolean isWhiteList(String method, String path) {
+        if (!Const.WHITE_LIST.containsKey(method)) {
+            return false;
+        }
+
+        String[] lists = Const.WHITE_LIST.get(method);
+        return PatternMatchUtils.simpleMatch(lists, path);
     }
 }
