85f89e8fc915dff1169473feca0ad2a5270b37b9
JoeMinKyung <mk981015@naver.com>
2025-04-07 10:55:54 +0900

    feat(#34): findAllByShowDateId 메서드 추가

--- src/main/java/com/example/picket/domain/seat/repository/SeatRepository.java
+++ src/main/java/com/example/picket/domain/seat/repository/SeatRepository.java
@@ -3,5 +3,9 @@ package com.example.picket.domain.seat.repository;
 import com.example.picket.domain.seat.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findAllByShowDateId(Long showDateId);
 }
