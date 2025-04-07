54fb3b33b9c0acb4ea294f22f5055462df034a41
JoeMinKyung <mk981015@naver.com>
2025-04-07 17:26:55 +0900

    feat(#31, #32, #33, #34): 유효성 검사 추가

--- src/main/java/com/example/picket/domain/seat/dto/SeatCreateRequest.java
+++ src/main/java/com/example/picket/domain/seat/dto/SeatCreateRequest.java
@@ -1,6 +1,9 @@
 package com.example.picket.domain.seat.dto;
 
 import com.example.picket.common.enums.Grade;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -8,7 +11,13 @@ import java.math.BigDecimal;
 @Getter
 public class SeatCreateRequest {
 
+    @NotNull(message = "Grade는 필수 입력값입니다.")
     private Grade grade; // VIP, A, R ...
+
+    @Min(value = 1, message = "좌석 수는 1개 이상이어야 합니다.")
     private int seatCount;
+
+    @NotNull(message = "가격은 필수 입력값입니다.")
+    @Positive(message = "가격은 0보다 커야 합니다.")
     private BigDecimal price;
 }
--- src/main/java/com/example/picket/domain/seat/dto/SeatUpdateRequest.java
+++ src/main/java/com/example/picket/domain/seat/dto/SeatUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.example.picket.domain.seat.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -7,7 +10,13 @@ import java.math.BigDecimal;
 @Getter
 public class SeatUpdateRequest {
 
+    @NotNull(message = "등급(grade)은 필수 입력값입니다.")
     private String grade;       // 등급 (예: VIP)
+
+    @Min(value = 1, message = "좌석 수는 1개 이상이어야 합니다.")
     private int quantity;       // 좌석 수
+
+    @NotNull(message = "가격은 필수 입력값입니다.")
+    @Positive(message = "가격은 0보다 커야 합니다.")
     private BigDecimal price;   // 좌석 가격
 }
