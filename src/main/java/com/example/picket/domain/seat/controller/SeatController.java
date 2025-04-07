8be8ff1eb8492ee151441135fa84f3be13a4f4b3
JoeMinKyung <mk981015@naver.com>
2025-04-07 15:22:03 +0900

    chore(#31, #32, #33, #34): builder 삭제 및 mapper 추가 및 entity로 받기

--- src/main/java/com/example/picket/domain/seat/controller/SeatController.java
+++ src/main/java/com/example/picket/domain/seat/controller/SeatController.java
@@ -4,8 +4,10 @@ import com.example.picket.common.annotation.Auth;
 import com.example.picket.common.dto.AuthUser;
 import com.example.picket.domain.seat.dto.SeatGroupByGradeResponse;
 import com.example.picket.domain.seat.dto.SeatUpdateRequest;
+import com.example.picket.domain.seat.entity.Seat;
 import com.example.picket.domain.seat.service.SeatCommandService;
 import com.example.picket.domain.seat.service.SeatQueryService;
+import com.example.picket.domain.seat.service.SeatResponseMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ public class SeatController {
 
     private final SeatQueryService seatQueryService;
     private final SeatCommandService seatCommandService;
+    private final SeatResponseMapper seatResponseMapper;
 
     // 좌석 상세 조회
     @GetMapping("/shows/{showId}/dates/{showDateId}/seats")
@@ -26,19 +29,21 @@ public class SeatController {
             @PathVariable Long showId,
             @PathVariable Long showDateId
     ) {
-        List<SeatGroupByGradeResponse> response = seatQueryService.getSeatsByShowDate(showDateId);
+        List<Seat> seats = seatQueryService.getSeatsByShowDate(showDateId);
+        List<SeatGroupByGradeResponse> response = seatResponseMapper.toGroupByGradeResponses(seats);
         return ResponseEntity.ok(response);
     }
 
     // 좌석 수정
     @PatchMapping("/shows/{showId}/dates/{showDateId}/seats")
-    public ResponseEntity<Void> updateSeats(
+    public ResponseEntity<List<SeatGroupByGradeResponse>> updateSeats(
             @Auth AuthUser authUser,
             @PathVariable Long showDateId,
             @RequestBody List<SeatUpdateRequest> requests
     ) {
-        seatCommandService.updateSeats(authUser, showDateId, requests);
-        return ResponseEntity.ok().build();
+        List<Seat> updatedSeats = seatCommandService.updateSeats(authUser, showDateId, requests);
+        List<SeatGroupByGradeResponse> response = seatResponseMapper.toGroupByGradeResponses(updatedSeats);
+        return ResponseEntity.ok(response);
     }
 
     // 좌석 삭제
@@ -51,4 +56,3 @@ public class SeatController {
         return ResponseEntity.noContent().build();
     }
 }
-
