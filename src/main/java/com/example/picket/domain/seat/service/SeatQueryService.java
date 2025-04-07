baf195c1b0672a9130d20f07dbbe33fdcf0e4370
JoeMinKyung <mk981015@naver.com>
2025-04-07 15:24:27 +0900

    chore(#34): refactor

--- src/main/java/com/example/picket/domain/seat/service/SeatQueryService.java
+++ src/main/java/com/example/picket/domain/seat/service/SeatQueryService.java
@@ -1,15 +1,11 @@
 package com.example.picket.domain.seat.service;
 
-import com.example.picket.common.enums.Grade;
-import com.example.picket.domain.seat.dto.SeatGroupByGradeResponse;
 import com.example.picket.domain.seat.entity.Seat;
 import com.example.picket.domain.seat.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,21 +13,9 @@ public class SeatQueryService {
 
     private final SeatRepository seatRepository;
 
-    public List<SeatGroupByGradeResponse> getSeatsByShowDate(Long showDateId) {
-
-        List<Seat> seats = seatRepository.findAllByShowDateId(showDateId);
-
-        return seats.stream()
-                .collect(Collectors.groupingBy(seat -> seat.getGrade()))
-                .entrySet()
-                .stream()
-                .map(entry -> {
-                    Grade grade = entry.getKey();
-                    List<Seat> groupedSeats = entry.getValue();
-                    BigDecimal price = groupedSeats.get(0).getPrice(); // 등급별 가격 동일하니까 첫 번째 꺼
-                    return SeatGroupByGradeResponse.from(grade, price, groupedSeats);
-                })
-                .toList();
+    public List<Seat> getSeatsByShowDate(Long showDateId) {
+        return seatRepository.findAllByShowDateId(showDateId);
     }
 }
+
 
