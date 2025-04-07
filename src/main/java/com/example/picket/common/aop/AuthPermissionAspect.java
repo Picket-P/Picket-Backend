939a47b21cacf9d4eb42551349ae529a2777b749
JoeMinKyung <mk981015@naver.com>
2025-04-07 18:04:23 +0900

    chore: 매개변수 수정

--- src/main/java/com/example/picket/common/aop/AuthPermissionAspect.java
+++ src/main/java/com/example/picket/common/aop/AuthPermissionAspect.java
@@ -4,17 +4,13 @@ import com.example.picket.common.annotation.AuthPermission;
 import com.example.picket.common.dto.AuthUser;
 import com.example.picket.common.enums.UserRole;
 import com.example.picket.common.exception.CustomException;
-import com.example.picket.common.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -23,8 +19,8 @@ public class AuthPermissionAspect {
 
     private final HttpServletRequest request;
 
-    @Before("@annotation(com.example.picket.common.annotation.AuthPermission)")
-    public void checkPermission(org.aspectj.lang.JoinPoint joinPoint) {
+    @Before("@annotation(authPermission)")
+    public void checkPermission(org.aspectj.lang.JoinPoint joinPoint, AuthPermission authPermission) {
         HttpSession session = request.getSession(false);
         if (session == null) {
             throw new CustomException(HttpStatus.FORBIDDEN, "이 작업을 수행할 권한이 없습니다.");
@@ -34,9 +30,6 @@ public class AuthPermissionAspect {
         if (authUser == null) {
             throw new CustomException(HttpStatus.FORBIDDEN, "이 작업을 수행할 권한이 없습니다.");
         }
-
-        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-        AuthPermission authPermission = method.getAnnotation(AuthPermission.class);
 
         UserRole requiredRole = authPermission.role();
         if (!authUser.getUserRole().equals(requiredRole)) {
