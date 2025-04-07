18e53fc7f4a72d869fffa3eb53e660c559c2e73d
JoeMinKyung <mk981015@naver.com>
2025-04-07 17:57:15 +0900

    feat(#3, #6, #20, #21, #35): show와 관련된 데이터를 같이 가져오게 수정

--- src/main/java/com/example/picket/domain/show/repository/ShowDateRepository.java
+++ src/main/java/com/example/picket/domain/show/repository/ShowDateRepository.java
@@ -2,6 +2,7 @@ package com.example.picket.domain.show.repository;
 
 import com.example.picket.domain.show.entity.Show;
 import com.example.picket.domain.show.entity.ShowDate;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.Optional;
 
 public interface ShowDateRepository extends JpaRepository<ShowDate, Long> {
 
+    @EntityGraph(attributePaths = {"show"}) // Show와 관련된 데이터를 fetch join으로 미리 로드
     List<ShowDate> findAllByShowId(Long showId);
+
+    @EntityGraph(attributePaths = {"show"}) // Show와 관련된 데이터를 fetch join으로 미리 로드
     Optional<ShowDate> findByShow(Show show);
 }
