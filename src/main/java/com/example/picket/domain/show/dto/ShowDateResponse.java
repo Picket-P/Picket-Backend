cdf1afba44c5193b87dea854ce41be908fab3ed1
JoeMinKyung <mk981015@naver.com>
2025-04-07 10:57:36 +0900

    feat(#3): show 조회시 좌석 정보 요약 응답

--- src/main/java/com/example/picket/domain/show/dto/ShowDateResponse.java
+++ src/main/java/com/example/picket/domain/show/dto/ShowDateResponse.java
@@ -1,11 +1,14 @@
 package com.example.picket.domain.show.dto;
 
+import com.example.picket.domain.seat.dto.SeatSummaryResponse;
 import com.example.picket.domain.show.entity.ShowDate;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @ToString
@@ -18,6 +21,7 @@ public class ShowDateResponse {
     private final Integer totalSeatCount;
     private final Integer reservedSeatCount;
     private final Integer availableSeatCount;
+    private final List<SeatSummaryResponse> seatSummary;
 
     public ShowDateResponse(ShowDate showDate) {
         this.id = showDate.getId();
@@ -27,9 +31,15 @@ public class ShowDateResponse {
         this.totalSeatCount = showDate.getTotalSeatCount();
         this.reservedSeatCount = showDate.getReservedSeatCount();
         this.availableSeatCount = showDate.getAvailableSeatCount();
+        this.seatSummary = new ArrayList<>();
     }
 
     public static ShowDateResponse from(ShowDate showDate) {
         return new ShowDateResponse(showDate);
+    }
+
+    public void setSeatSummary(List<SeatSummaryResponse> summaryList) {
+        this.seatSummary.clear();
+        this.seatSummary.addAll(summaryList);
     }
 }
