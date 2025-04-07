5dc4aa200e02689df0659c5ccfeed25a40fbc534
JoeMinKyung <mk981015@naver.com>
2025-04-07 20:52:24 +0900

    feat(#31, #32, #33, #34): show와 seat 연관관계 설정

--- src/main/java/com/example/picket/domain/seat/entity/Seat.java
+++ src/main/java/com/example/picket/domain/seat/entity/Seat.java
@@ -2,6 +2,7 @@ package com.example.picket.domain.seat.entity;
 
 import com.example.picket.common.enums.Grade;
 import com.example.picket.common.enums.SeatStatus;
+import com.example.picket.domain.show.entity.Show;
 import com.example.picket.domain.show.entity.ShowDate;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -35,19 +36,33 @@ public class Seat {
     private BigDecimal price;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "show_id", nullable = false)
+    private Show show;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "show_date_id", nullable = false)
     private ShowDate showDate;
 
-    private Seat(Grade grade, Integer seatNumber, BigDecimal price, SeatStatus seatStatus, ShowDate showDate) {
+    private Seat(Grade grade, Integer seatNumber, BigDecimal price, SeatStatus seatStatus, ShowDate showDate, Show show) {
+        if (!showDate.getShow().equals(show)) {
+            throw new IllegalArgumentException("ShowDate의 Show와 전달받은 Show가 일치하지 않습니다.");
+        }
         this.grade = grade;
         this.seatNumber = seatNumber;
         this.price = price;
         this.seatStatus = seatStatus;
         this.showDate = showDate;
+        this.show = show;
     }
 
     public static Seat toEntity(Grade grade, int seatNumber, BigDecimal price, ShowDate showDate) {
-        return new Seat(grade, seatNumber, price, SeatStatus.AVAILABLE, showDate);
+        Show show = showDate.getShow();
+
+        if (show == null) {
+            throw new IllegalStateException("ShowDate에 연결된 Show가 없습니다.");
+        }
+
+        return new Seat(grade, seatNumber, price, SeatStatus.AVAILABLE, showDate, show);
     }
 
     public void updatePrice(BigDecimal price) {
