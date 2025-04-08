09d69cb42d091924199f489b64a9ce96ed23e4be
Moon YuBin <yubin5744@gmail.com>
2025-04-08 11:20:13 +0900

    refactor(#2) : 티켓 생성 시 N+1 문제 해결, SeatRepository에 JPQL @Param 추가, yml파일에 로그 보이도록 수정

--- src/main/java/com/example/picket/domain/seat/repository/SeatRepository.java
+++ src/main/java/com/example/picket/domain/seat/repository/SeatRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     List<Seat> findAllByShowDateId(Long showDateId);
 
-    @Query("SELECT s FROM Seat s JOIN FETCH s.showDate sd JOIN FETCH sd.show WHERE s.id = :id")
-    Optional<Seat> findByIdWithShowDateAndShow(Long id);
+    @Query("SELECT s FROM Seat s JOIN FETCH s.showDate sd JOIN FETCH sd.show WHERE s.id = :seatId")
+    Optional<Seat> findByIdWithShowDateAndShow(@Param("seatId") Long seatId);
 }
