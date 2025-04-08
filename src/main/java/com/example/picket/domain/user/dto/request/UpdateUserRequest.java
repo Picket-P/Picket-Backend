88b6766e46c74286fc308e45eaba1c931a652f0b
dawon <aodlstory321@gmail.com>
2025-04-08 20:21:25 +0900

    feat: Swagger 적용

--- src/main/java/com/example/picket/domain/user/dto/request/UpdateUserRequest.java
+++ src/main/java/com/example/picket/domain/user/dto/request/UpdateUserRequest.java
@@ -1,10 +1,23 @@
 package com.example.picket.domain.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UpdateUserRequest {
+
+    @Schema(description = "비밀번호", example = "기존 비밀번호입니다.")
+    @NotBlank
     private String password;
+
+    @Schema(description = "닉네임", example = "변경할 닉네임입니다.")
+    @NotBlank
     private String nickname;
+
+    @Schema(description = "프로필", example = "새롭게 변경할 프로필 URL입니다.")
+    @NotBlank
     private String profileUrl;
 }
