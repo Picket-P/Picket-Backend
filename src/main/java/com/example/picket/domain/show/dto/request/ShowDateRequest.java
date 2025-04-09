1ac0b3b520de45d5ecaa385db79884c1614fe47c
JiHo Jeon <wjswlgh96@naver.com>
2025-04-09 15:18:30 +0900

    feat(#49): Show Swagger 적용

--- src/main/java/com/example/picket/domain/show/dto/request/ShowDateRequest.java
+++ src/main/java/com/example/picket/domain/show/dto/request/ShowDateRequest.java
@@ -2,6 +2,7 @@ package com.example.picket.domain.show.dto.request;
 
 import com.example.picket.domain.seat.dto.request.SeatCreateRequest;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -17,22 +18,25 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ShowDateRequest {
 
+    @Schema(description = "공연 날짜 (형식: yyyy-MM-dd)", example = "2025-05-12")
     @NotNull(message = "공연 날짜는 필수 입력값입니다.")
     @FutureOrPresent(message = "공연 날짜는 오늘 이후여야 합니다.")
     private LocalDate date;
 
+    @Schema(description = "공연 시작 시간 (형식: HH:mm:ss)", example = "14:00:00")
     @NotNull(message = "공연 시작 시간은 필수 입력값입니다.")
-    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime startTime;
 
+    @Schema(description = "공연 종료 시간 (형식: HH:mm:ss)", example = "16:00:00")
     @NotNull(message = "공연 종료 시간은 필수 입력값입니다.")
-    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime endTime;
 
+    @Schema(description = "총 좌석 수", example = "100")
     @NotNull(message = "좌석 수는 필수 입력값입니다.")
     @Min(value = 1, message = "좌석 수는 1 이상이어야 합니다.")
     private Integer totalSeatCount;
 
+    @Schema(description = "좌석 생성 요청 리스트", example = "[{\"grade\": \"VIP\", \"seatCount\": 100, \"price\": 50000}]")
     private List<SeatCreateRequest> seatCreateRequests;
 
 }
--- src/main/java/com/example/picket/domain/show/dto/request/ShowUpdateRequest.java
+++ src/main/java/com/example/picket/domain/show/dto/request/ShowUpdateRequest.java
@@ -4,6 +4,7 @@ import com.example.picket.common.enums.Category;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,28 +14,34 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ShowUpdateRequest {
 
+    @Schema(description = "제목", example = "수정한 공연 제목")
     @NotBlank(message = "공연 제목은 필수 입력 값입니다.")
     private String title;
 
+    @Schema(description = "제목", example = "http://aws.s3/project/changePoster.jpg")
     @NotBlank(message = "포스터 URL은 필수 입력 값입니다.")
     private String posterUrl;
 
+    @Schema(description = "카테고리", example = "CONCERT")
     @NotNull(message = "카테고리는 필수 입력 값입니다.")
     private Category category;
 
+    @Schema(description = "공연 설명", example = "수정한 공연 설명입니다.")
     @NotBlank(message = "공연 설명은 필수 입력 값입니다.")
     private String description;
 
+    @Schema(description = "공연 설명", example = "경기도 어딘가")
     @NotBlank(message = "공연 장소는 필수 입력 값입니다.")
     private String location;
 
+    @Schema(description = "예매 시작일 (형식: yyyy-MM-dd'T'HH:mm:ss)", example = "2025-05-02T00:00:00")
     @NotNull(message = "예매 시작일은 필수 입력 값입니다.")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime reservationStart;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @Schema(description = "예매 종료일 (형식: yyyy-MM-dd'T'HH:mm:ss, null이면 공연시작 전날 자정으로 설정)", example = "2025-05-09T00:00:00")
     private LocalDateTime reservationEnd;
 
+    @Schema(description = "인당 최대 구매 가능 티켓수", example = "100")
     private Integer ticketsLimitPerUser;
 
 }
