08af2da0ffb56cc9e54ff5549623f001bc3cbe57
JoeMinKyung <mk981015@naver.com>
2025-04-15 16:58:05 +0900

    chore(#89): swagger 적용 및 url 수정

--- src/main/java/com/example/picket/domain/ranking/controller/RankingController.java
+++ src/main/java/com/example/picket/domain/ranking/controller/RankingController.java
@@ -1,5 +1,6 @@
 package com.example.picket.domain.ranking.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
@@ -13,24 +14,27 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/api/v1/rankings")
+@RequestMapping("/api/v2/rankings")
 @RequiredArgsConstructor
 public class RankingController {
 
     private final RedisTemplate<String, String> redisTemplate;
 
+    @Operation(summary = "인기 검색어 랭킹", description = "인기 검색어 랭킹을 조회할 수 있습니다.")
     @GetMapping("/popular-keywords")
     public ResponseEntity<List<String>> getPopularKeywords() {
         List<String> keywords = redisTemplate.opsForList().range("ranking:popular_keywords", 0, -1);
         return ResponseEntity.ok(keywords != null ? keywords : Collections.emptyList());
     }
 
+    @Operation(summary = "조회수 랭킹", description = "조회수 랭킹을 조회할 수 있습니다.")
     @GetMapping("/hot-shows")
     public ResponseEntity<List<String>> getHotShows() {
         Set<String> shows = redisTemplate.opsForZSet().reverseRange("ranking:hot_shows", 0, 9);
         return ResponseEntity.ok(new ArrayList<>(shows != null ? shows : Collections.emptySet()));
     }
 
+    @Operation(summary = "좋아요 랭킹", description = "좋아요 랭킹을 조회할 수 있습니다.")
     @GetMapping("/like-shows")
     public ResponseEntity<List<String>> getLikeShows() {
         Set<String> shows = redisTemplate.opsForZSet().reverseRange("ranking:like_shows", 0, 9);
