558dae7d6891a93b420c2397345abd609dceb6a1
JoeMinKyung <mk981015@naver.com>
2025-04-15 18:55:10 +0900

    chore(#89): conflict merge

--- src/main/java/com/example/picket/domain/show/controller/ShowController.java
+++ src/main/java/com/example/picket/domain/show/controller/ShowController.java
@@ -6,6 +6,7 @@ import com.example.picket.common.dto.AuthUser;
 import com.example.picket.common.dto.PageResponse;
 import com.example.picket.common.enums.Category;
 import com.example.picket.common.enums.UserRole;
+import com.example.picket.domain.ranking.scheduler.PopularKeywordScheduler;
 import com.example.picket.domain.show.dto.request.ShowCreateRequest;
 import com.example.picket.domain.show.dto.request.ShowUpdateRequest;
 import com.example.picket.domain.show.dto.response.ShowDateDetailResponse;
@@ -34,6 +35,7 @@ public class ShowController {
     private final ShowCommandService showCommandService;
     private final ShowQueryService showQueryService;
     private final ShowDateQueryService showDateQueryService;
+    private final PopularKeywordScheduler popularKeywordScheduler;
 
     // 공연 생성
     @AuthPermission(role = UserRole.DIRECTOR)
