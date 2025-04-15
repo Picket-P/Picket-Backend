8714efc4cdd697dfa4bbe35b18fe8587323e89ca
Moon YuBin <yubin5744@gmail.com>
2025-04-15 23:00:52 +0900

    feat(#93) : Transactional 추가, booking 함수 분리, createOrder 수정

--- src/main/java/com/example/picket/domain/booking/controller/BookingController.java
+++ src/main/java/com/example/picket/domain/booking/controller/BookingController.java
@@ -6,6 +6,8 @@ import com.example.picket.domain.booking.dto.request.BookingRequest;
 import com.example.picket.domain.booking.service.BookingService;
 import com.example.picket.domain.order.dto.response.OrderResponse;
 import com.example.picket.domain.order.entity.Order;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,11 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/v2/")
 @RequiredArgsConstructor
+@Tag(name = "예매 API", description = "예매, 예매취소 기능 API입니다.")
 public class BookingController {
 
     private final BookingService bookingService;
 
     // 예매 API
+    @Operation(summary = "예매", description = "좌석을 예매하여 티켓, 주문을 생성할 수 있습니다.")
     @PostMapping("show/{showId}/show-date/{showDateId}/booking")
     public ResponseEntity<OrderResponse> booking(
             @PathVariable Long showId,
