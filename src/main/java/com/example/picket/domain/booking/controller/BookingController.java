18a41b8a9cacbc4cb2601b47bf349f2f86d83199
Moon YuBin <yubin5744@gmail.com>
2025-04-16 00:34:53 +0900

    feat(#94) : 예매 취소 API 구현

--- src/main/java/com/example/picket/domain/booking/controller/BookingController.java
+++ src/main/java/com/example/picket/domain/booking/controller/BookingController.java
@@ -3,18 +3,22 @@ package com.example.picket.domain.booking.controller;
 import com.example.picket.common.annotation.Auth;
 import com.example.picket.common.dto.AuthUser;
 import com.example.picket.domain.booking.dto.request.BookingRequest;
+import com.example.picket.domain.booking.dto.request.CancelBookingRequest;
+import com.example.picket.domain.booking.dto.response.CancelBookingResponse;
 import com.example.picket.domain.booking.service.BookingService;
 import com.example.picket.domain.order.dto.response.OrderResponse;
 import com.example.picket.domain.order.entity.Order;
+import com.example.picket.domain.ticket.dto.response.GetTicketResponse;
+import com.example.picket.domain.ticket.entity.Ticket;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/api/v2/")
@@ -35,5 +39,18 @@ public class BookingController {
         Order order = bookingService.booking(showId, showDateId, authUser.getId(), dto.getSeatIds());
         OrderResponse orderResponse = OrderResponse.toDto(order);
         return ResponseEntity.ok(orderResponse);
+    }
+
+    @Operation(summary = "예매취소", description = "예매된 티켓을 취소할 수 있습니다.")
+    @DeleteMapping("show/{showId}/show-date/{showDateId}/booking")
+    public ResponseEntity<CancelBookingResponse> cancelBooking(
+            @PathVariable Long showId,
+            @PathVariable Long showDateId,
+            @Auth AuthUser authUser,
+            @RequestBody CancelBookingRequest dto
+    ) throws InterruptedException {
+        List<Ticket> canceledTickets = bookingService.cancelBooking(showId, showDateId, authUser.getId(), dto.getTicketIds());
+        CancelBookingResponse cancelBookingResponse = CancelBookingResponse.toDto(canceledTickets);
+        return ResponseEntity.ok(cancelBookingResponse);
     }
 }
---
+++ src/main/java/com/example/picket/domain/booking/dto/request/CancelBookingRequest.java
@@ -0,0 +1,16 @@
+package com.example.picket.domain.booking.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class CancelBookingRequest {
+
+    @Schema(description = "취소할 티켓 ID", example = "[1]")
+    @NotNull(message = "티켓 ID는 필수 입력값입니다.")
+    private List<Long> ticketIds = new ArrayList<>();
+}
