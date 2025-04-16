ff177984bde58a23a23ff1726ff38e647f0fdd95
Moon YuBin <yubin5744@gmail.com>
2025-04-16 10:41:00 +0900

    feat(#93, #94) : 좌석 조회 API AVAILABLE만 가능하게 수정

--- src/main/java/com/example/picket/domain/seat/repository/SeatRepository.java
+++ src/main/java/com/example/picket/domain/seat/repository/SeatRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
-    List<Seat> findAllByShowDateId(Long showDateId);
+    @Query("SELECT s FROM Seat s WHERE s.showDate.id = :showDateId AND s.seatStatus = 'AVAILABLE'")
+    List<Seat> findAllByShowDateId(@Param("showDateId") Long showDateId);
 
     @Query("SELECT s FROM Seat s JOIN FETCH s.showDate sd JOIN FETCH sd.show WHERE s.id = :seatId")
     Optional<Seat> findByIdWithShowDateAndShow(@Param("seatId") Long seatId);
