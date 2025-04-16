f420f0aa45879a8d3fee253448c4d21559bcffc4
Moon YuBin <yubin5744@gmail.com>
2025-04-16 11:27:02 +0900

    feat(#95) : 주문 다건, 단건 조회 API 구현

--- src/main/java/com/example/picket/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/picket/domain/order/repository/OrderRepository.java
@@ -1,15 +1,23 @@
 package com.example.picket.domain.order.repository;
 
 import com.example.picket.domain.order.entity.Order;
+import com.example.picket.domain.ticket.entity.Ticket;
 import com.example.picket.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @EntityGraph(attributePaths = "user, ticket")
-    Optional<Order> findByUser(User user);
+    @EntityGraph(attributePaths = {"user", "ticket"})
+    Optional<Order> findByIdAndUser(Long orderId, User user);
+
+    @EntityGraph(attributePaths = {"user", "ticket"})
+    Page<Order> findByUser(User user, Pageable pageable);
 
 }
--- src/main/java/com/example/picket/domain/order/service/OrderQueryService.java
+++ src/main/java/com/example/picket/domain/order/service/OrderQueryService.java
@@ -6,8 +6,14 @@ import com.example.picket.domain.order.repository.OrderRepository;
 import com.example.picket.domain.user.entity.User;
 import com.example.picket.domain.user.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -20,11 +26,17 @@ public class OrderQueryService {
 
     @Transactional(readOnly = true)
     public Order getOrder(Long userId, Long orderId) {
-
         User user = userQueryService.getUser(userId);
-
-        Order foundOrder = orderRepository.findByUser(user).orElseThrow(
+        return orderRepository.findByIdAndUser(orderId, user).orElseThrow(
                 () -> new CustomException(NOT_FOUND, "존재하지 않는 Order입니다."));
-        return foundOrder;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Order> getOrders(int size, int page, Long userId) {
+        User user = userQueryService.getUser(userId);
+        Sort sortStandard = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page - 1, size, sortStandard);
+
+        return orderRepository.findByUser(user, pageable);
     }
 }
