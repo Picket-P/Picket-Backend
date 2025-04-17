f1adcc83205c2fe8c0bdaa334ac2377de62e7560
JoeMinKyung <mk981015@naver.com>
2025-04-17 19:12:22 +0900

    feat(#89): 좋아요 랭킹

---
+++ src/main/java/com/example/picket/domain/ranking/entity/LikeShow.java
@@ -0,0 +1,36 @@
+package com.example.picket.domain.ranking.entity;
+
+import com.example.picket.common.enums.ShowStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@NoArgsConstructor
+public class LikeShow {
+    private Long showId;
+    private String title;
+    private Long likeCount;
+    private String status;
+    private LocalDateTime createdAt;
+
+    private LikeShow(Long showId, String title, Long likeCount, ShowStatus status, LocalDateTime createdAt) {
+        this.showId = showId;
+        this.title = title;
+        this.likeCount = likeCount;
+        this.status = status.name();
+        this.createdAt = createdAt;
+    }
+
+    public static LikeShow toEntity(Long showId, String title, Long likeCount, ShowStatus status, LocalDateTime createdAt) {
+        LikeShow show = new LikeShow();
+        show.showId = showId;
+        show.title = title;
+        show.likeCount = likeCount;
+        show.status = status.name();
+        show.createdAt = createdAt;
+        return show;
+    }
+}
