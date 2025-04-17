b6c41fcf2283f34d238663ddbfb7eccc574c3ef3
JoeMinKyung <mk981015@naver.com>
2025-04-17 18:28:20 +0900

    refactor(#110): import 정리

--- src/main/java/com/example/picket/domain/show/entity/ShowDate.java
+++ src/main/java/com/example/picket/domain/show/entity/ShowDate.java
@@ -1,25 +1,12 @@
 package com.example.picket.domain.show.entity;
 
-import static org.springframework.http.HttpStatus.CONFLICT;
-
 import com.example.picket.common.entity.BaseEntity;
-import com.example.picket.common.exception.CustomException;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "show_dates")
