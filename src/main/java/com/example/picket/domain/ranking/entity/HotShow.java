9817230a20923e4d3d3431344163ccfea41cdf9d
JoeMinKyung <mk981015@naver.com>
2025-04-17 18:32:56 +0900

    feat(#89): HotShow Entity

---
+++ src/main/java/com/example/picket/domain/ranking/entity/HotShow.java
@@ -0,0 +1,28 @@
+package com.example.picket.domain.ranking.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class HotShow {
+    private Long showId;
+    private String title;
+    private Long viewCount;
+    private String status;
+    private LocalDateTime createdAt;
+
+    private HotShow(Long showId, String title, Long viewCount, String status, LocalDateTime createdAt) {
+        this.showId = showId;
+        this.title = title;
+        this.viewCount = viewCount;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public static HotShow toEntity(Long showId, String title, Long viewCount, String status, LocalDateTime createdAt) {
+        return new HotShow(showId, title, viewCount, status, createdAt);
+    }
+}
