eb1b670a6b054e3d29942f60423e1f3dd34cbf00
JoeMinKyung <mk981015@naver.com>
2025-04-17 18:23:25 +0900

    feat(#89): 조회순 랭킹 출력 쿼리문 추가

--- src/main/java/com/example/picket/domain/show/repository/ShowRepository.java
+++ src/main/java/com/example/picket/domain/show/repository/ShowRepository.java
@@ -1,6 +1,7 @@
 package com.example.picket.domain.show.repository;
 
 import com.example.picket.common.enums.Category;
+import com.example.picket.common.enums.ShowStatus;
 import com.example.picket.domain.show.entity.Show;
 import com.example.picket.domain.show.repository.querydsl.ShowQueryDslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +14,9 @@ import java.util.List;
 public interface ShowRepository extends JpaRepository<Show, Long>, ShowQueryDslRepository {
 
     List<Show> findAllByCategoryAndDeletedAtIsNull(Category category);
-    List<Show> findTop10ByOrderByViewCountDesc();
+
+    @Query("SELECT s FROM Show s WHERE s.status != :status AND s.deletedAt IS NULL ORDER BY s.viewCount DESC")
+    List<Show> findTop10ByStatusNotAndOrderByViewCountDesc(ShowStatus status);
 
     @Modifying
     @Query("UPDATE Show s SET s.viewCount = s.viewCount + 1 WHERE s.id = :showId AND s.deletedAt IS NULL")
