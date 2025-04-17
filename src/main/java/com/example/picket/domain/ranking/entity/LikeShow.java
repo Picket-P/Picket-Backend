6df73ece25415001da242ac0d3bbead9d8712581
JoeMinKyung <mk981015@naver.com>
2025-04-18 03:02:06 +0900

    refactor(#89): likeCount Long -> int, status String -> ShowStatus

--- src/main/java/com/example/picket/domain/ranking/entity/LikeShow.java
+++ src/main/java/com/example/picket/domain/ranking/entity/LikeShow.java
@@ -12,24 +12,24 @@ import java.time.LocalDateTime;
 public class LikeShow {
     private Long showId;
     private String title;
-    private Long likeCount;
-    private String status;
+    private int likeCount;
+    private ShowStatus status;
     private LocalDateTime createdAt;
 
-    private LikeShow(Long showId, String title, Long likeCount, ShowStatus status, LocalDateTime createdAt) {
+    private LikeShow(Long showId, String title, int likeCount, ShowStatus status, LocalDateTime createdAt) {
         this.showId = showId;
         this.title = title;
         this.likeCount = likeCount;
-        this.status = status.name();
+        this.status = status;
         this.createdAt = createdAt;
     }
 
-    public static LikeShow toEntity(Long showId, String title, Long likeCount, ShowStatus status, LocalDateTime createdAt) {
+    public static LikeShow toEntity(Long showId, String title, int likeCount, ShowStatus status, LocalDateTime createdAt) {
         LikeShow show = new LikeShow();
         show.showId = showId;
         show.title = title;
         show.likeCount = likeCount;
-        show.status = status.name();
+        show.status = status;
         show.createdAt = createdAt;
         return show;
     }
