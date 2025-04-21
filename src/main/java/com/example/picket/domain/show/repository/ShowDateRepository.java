f4bddfd93539915a436c69f7f95292d87bc4cbaa
JoeMinKyung <mk981015@naver.com>
2025-04-21 14:06:16 +0900

    feat(#109): param으로 수정

--- src/main/java/com/example/picket/domain/show/repository/ShowDateRepository.java
+++ src/main/java/com/example/picket/domain/show/repository/ShowDateRepository.java
@@ -1,11 +1,13 @@
 package com.example.picket.domain.show.repository;
 
+import com.example.picket.common.enums.ShowStatus;
 import com.example.picket.domain.show.entity.Show;
 import com.example.picket.domain.show.entity.ShowDate;
 import com.example.picket.domain.show.repository.querydsl.ShowDateQueryDslRepository;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,7 +20,7 @@ public interface ShowDateRepository extends JpaRepository<ShowDate, Long>, ShowD
 
     Optional<ShowDate> findShowDateByShow(Show show);
 
-    @Query("SELECT sd FROM ShowDate sd WHERE sd.show.status != com.example.picket.common.enums.ShowStatus.FINISHED " +
+    @Query("SELECT sd FROM ShowDate sd WHERE sd.show.status != :status " +
             "AND sd.deletedAt IS NULL")
-    List<ShowDate> findAllActiveShowDates();
+    List<ShowDate> findAllActiveShowDates(@Param("status") ShowStatus status);
 }
