ffbb84bed16133d8554145ae710adb5f826f1a0a
JoeMinKyung <mk981015@naver.com>
2025-04-21 15:41:21 +0900

    feat(#89, #109): status 추가

--- src/main/java/com/example/picket/domain/ranking/dto/response/HotShowResponse.java
+++ src/main/java/com/example/picket/domain/ranking/dto/response/HotShowResponse.java
@@ -1,5 +1,6 @@
 package com.example.picket.domain.ranking.dto.response;
 
+import com.example.picket.common.enums.ShowStatus;
 import com.example.picket.domain.ranking.entity.HotShow;
 import lombok.Getter;
 
@@ -10,16 +11,18 @@ public class HotShowResponse {
     private final Long showId;
     private final String title;
     private final int viewCount;
+    private final ShowStatus status;
     private final LocalDateTime createdAt;
 
-    private HotShowResponse(Long showId, String title, int viewCount, LocalDateTime createdAt) {
+    private HotShowResponse(Long showId, String title, int viewCount, ShowStatus status, LocalDateTime createdAt) {
         this.showId = showId;
         this.title = title;
         this.viewCount = viewCount;
+        this.status = status;
         this.createdAt = createdAt;
     }
 
     public static HotShowResponse toDto(HotShow hotShow) {
-        return new HotShowResponse(hotShow.getShowId(), hotShow.getTitle(), hotShow.getViewCount(), hotShow.getCreatedAt());
+        return new HotShowResponse(hotShow.getShowId(), hotShow.getTitle(), hotShow.getViewCount(), hotShow.getStatus(), hotShow.getCreatedAt());
     }
 }
--- src/main/java/com/example/picket/domain/ranking/dto/response/LikeShowResponse.java
+++ src/main/java/com/example/picket/domain/ranking/dto/response/LikeShowResponse.java
@@ -1,8 +1,8 @@
 package com.example.picket.domain.ranking.dto.response;
 
+import com.example.picket.common.enums.ShowStatus;
 import com.example.picket.domain.ranking.entity.LikeShow;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -11,16 +11,18 @@ public class LikeShowResponse {
     private final Long showId;
     private final String title;
     private final Long likeCount;
+    private final ShowStatus status;
     private final LocalDateTime createdAt;
 
-    private LikeShowResponse(Long showId, String title, Long likeCount, LocalDateTime createdAt) {
+    private LikeShowResponse(Long showId, String title, Long likeCount, ShowStatus status, LocalDateTime createdAt) {
         this.showId = showId;
         this.title = title;
         this.likeCount = likeCount;
+        this.status = status;
         this.createdAt = createdAt;
     }
 
     public static LikeShowResponse toDto(LikeShow likeShow) {
-        return new LikeShowResponse(likeShow.getShowId(), likeShow.getTitle(), likeShow.getLikeCount(), likeShow.getCreatedAt());
+        return new LikeShowResponse(likeShow.getShowId(), likeShow.getTitle(), likeShow.getLikeCount(), likeShow.getStatus(), likeShow.getCreatedAt());
     }
 }
