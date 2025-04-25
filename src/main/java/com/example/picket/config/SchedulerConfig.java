b44faf96f67b94b3605e0c19a54f1196a4c2e9f1
Moon YuBin <yubin5744@gmail.com>
2025-04-25 11:22:04 +0900

    refactor(#86) : 스케줄러 cron 표현식으로 수정, Thread Pool 설정

--- src/main/java/com/example/picket/config/SchedulerConfig.java
+++ src/main/java/com/example/picket/config/SchedulerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
@@ -14,5 +15,15 @@ public class SchedulerConfig {
     @Bean
     public RedisLockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
         return new RedisLockProvider(redisConnectionFactory);
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler configureScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5); // 스레드 풀 크기
+        threadPoolTaskScheduler.setThreadGroupName("scheduler thread pool"); // 스레드 그룹명
+        threadPoolTaskScheduler.setThreadNamePrefix("picket-scheduler-"); // 스레드명 접두사
+        threadPoolTaskScheduler.initialize(); // 스케줄러 초기화
+        return threadPoolTaskScheduler;
     }
 }
