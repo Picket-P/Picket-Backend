c119bed7cbe098ddbe176b5f0bf71fdcc590002a
cjh0412 <jyedev04@gmail.com>
2025-04-28 12:50:09 +0900

    test(#121): 이메일 인증 및 프로필 도입에 따른 통합 테스트 코드 수정

--- src/main/java/com/example/picket/common/dto/AuthUser.java
+++ src/main/java/com/example/picket/common/dto/AuthUser.java
@@ -7,8 +7,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AuthUser{
