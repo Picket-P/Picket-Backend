ca1fb5fec3cfcdd171d5bae03296929d999ea6a8
JEONTAK <tak980418@naver.com>
2025-04-29 16:58:49 +0900

    style : 패키지 이름 수정

--- src/main/java/com/example/picket/common/controller/RedirectController.java
+++ src/main/java/com/example/picket/common/controller/RedirectController.java
@@ -1,4 +1,4 @@
-package com.example.picket.common.Controller;
+package com.example.picket.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
